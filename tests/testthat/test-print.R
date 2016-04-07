d069a05434f01c1a0b00d5ffdc7c8a1481953642
Edwin de Jonge <edwindjonge@gmail.com>
2016-04-07 14:59:58 +0200

    update for testthat update

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -12,7 +12,10 @@ describe("print", {
 describe("print groups", {
   it("should print groups chunked info", {
     tbl_iris <- get_tbl_iris() %>% group_by(Species)
-    expect_warning(expect_output(print(tbl_iris), "^Source: chunked text file"))
-    expect_warning(expect_output(print(tbl_iris), "Groups: Species"))
+    suppressWarnings({
+      expect_output(print(tbl_iris), "^Source: chunked text file")
+      expect_output(print(tbl_iris), "Groups: Species")
+    })
+    #expect_warning(print(tbl_iris))
   })
 })
