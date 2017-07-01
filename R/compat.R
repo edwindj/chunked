34619772d7978a8edf6221c3464737fe02cc2bb0
Edwin de Jonge <edwindjonge@gmail.com>
2017-07-01 22:08:40 +0200

    fixing CRAN error

--- R/compat.R
+++ R/compat.R
@@ -1,10 +1,5 @@
 sql_render <- function (query)
 {
-  if (utils::packageVersion("dplyr") > "0.5.0") {
-    dplyr::check_dbplyr()
-    dbplyr::sql_render(query = query)
-  }
-  else {
-    dplyr::sql_render(query = query)
-  }
+  dplyr::check_dbplyr()
+  dbplyr::sql_render(query = query)
 }
